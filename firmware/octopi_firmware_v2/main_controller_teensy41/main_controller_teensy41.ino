5718c6929ca8ea60d996cbe2df6efcc14d677d28
MakerTobey <tobias-wenzel@users.noreply.github.com>
2026-01-19 12:48:46 -0300

    Update Teensy firmware configuration: switch to def_squid.h
    - Change from def_octopi_80120.h to def_squid.h
    - Enables SiMoRa-specific firmware configuration

--- firmware/octopi_firmware_v2/main_controller_teensy41/main_controller_teensy41.ino
+++ firmware/octopi_firmware_v2/main_controller_teensy41/main_controller_teensy41.ino
@@ -6,9 +6,9 @@
 #include "crc8.h"
 
 //#include "def_octopi.h"
-#include "def_octopi_80120.h"
+//#include "def_octopi_80120.h"
 //#include "def_gravitymachine.h"
-//#include "def_squid.h"
+#include "def_squid.h"
 //#include "def_platereader.h"
 //#include "def_squid_vertical.h"
 
